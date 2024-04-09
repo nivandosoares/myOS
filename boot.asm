c909c5d6ae2ab89917e91d0de4e13358eb3b5a23
Nivando Soares <sidvandoni@gmail.com>
2024-04-09 11:00:50 -0300

    #1 challenge: print alphabetical characteres but minor

--- boot.asm
+++ boot.asm
@@ -3,14 +3,14 @@ ORG 0x7C00           ; Indica o endereço de carga do bootloader
 
 start:
     mov ah, 0x0E     ; Função teletype para interrupção de vídeo
-    mov al, 'A'      ; Caractere inicial a imprimir
+    mov al, 'a'      ; Caractere inicial a imprimir
     mov bh, 0x00     ; Página de vídeo
     mov bl, 0x07     ; Atributos de cor (branco sobre preto)
 
 loop:
     int 0x10         ; Chama a interrupção de vídeo para imprimir AL
     inc al           ; Incrementa AL para o próximo caractere
-    cmp al, 'Z' + 1  ; Compara se já passou de 'Z'
+    cmp al, 'z' + 1  ; Compara se já passou de 'Z'
     jne loop         ; Se não, volta para o loop
 
 hang:
